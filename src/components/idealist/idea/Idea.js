ba2cd04eec9abef7d00df424be99538901dc2465
Manuel Kruisz <kruisz88@gmail.com>
2018-07-19 22:20:14 +0200

    add basic dragging to notes

--- src/components/idealist/idea/Idea.js
+++ src/components/idealist/idea/Idea.js
@@ -5,7 +5,9 @@ class Idea extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            idea: props.idea
+            idea: props.idea,
+            x: 0,
+            y: 0
         }
     }
 
@@ -25,10 +27,33 @@ class Idea extends Component {
         this.props.deleteIdea(id);
     }
 
+    dragStart = (evt) => {
+        console.log('dragStart');
+        let startX = evt.clientX;
+        let startY = evt.clientY;
+        this.setState({startX, startY});
+        console.log(this.state);
+    };
+
+    dragEnd = (evt) => {
+        console.log('dragEnd');
+        const {clientX, clientY} = evt;
+        this.setState((prevState) => {
+            return {
+                x: prevState.x + (clientX - this.state.startX),
+                y: prevState.y + (clientY - this.state.startY)
+            }
+        });
+    };
+
     render() {
         const {idea} = this.props;
         return (
-            <div className="idea">
+            <div className="idea" draggable="true" style={
+                {
+                    left: this.state.x,
+                    top: this.state.y
+                }}  >
                 <div className="idea-title">
                     <input className="idea-title-main"
                            value={idea.title}
@@ -43,6 +68,11 @@ class Idea extends Component {
                               rows='10'
                               value={idea.body}
                               onChange={this.bodyChange.bind(this)}/>
+                </div>
+                <div className="dragger" draggable="true"
+                     onDragStart={this.dragStart}
+                     onDrag={this.onDrag}
+                     onDragEnd={this.dragEnd}>
                 </div>
             </div>
         );
